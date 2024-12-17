99497a4c6cec63783cdd3f03f3bbdc0f3b5050bb
kelvinkapuya <kapuyakelvin18.kk@gmail.com>
2024-12-17 06:50:30 -0800

    Basic operations script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+number1 = 10 #Assign value to to variable number1
+number2 = 5 #Assign value to variable number2
+
+sum = number1 + number2 #summing the two variables
+subtract = number1 - number2 #subtracting the two variables
+multiply = number1 * number2 #multiplication of the two variables
+
+print("Addition of ", number1, "and ", number2, "is ", sum)
+print("Subtraction of ", number1, "and ", number2, "is ", subtract)
+print("Multiplication of ", number1, "and ", number2, "is ", multiply)
+
+
